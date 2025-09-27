3bae19783eb6af3f00bea8ea0aaeb342d1add5f2
shivkumar9693 <shivkumarthakur9693@gmail.com>
2025-09-27 22:37:50 +0530

    ServerSide Validation Added

--- ServerSideValidator/src/main/java/com/Main/Controller/myController.java
+++ ServerSideValidator/src/main/java/com/Main/Controller/myController.java
@@ -2,22 +2,32 @@ package com.Main.Controller;
 
  
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Main.Entity.User;
 
+import jakarta.validation.Valid;
+
 @Controller
 public class myController {
 	
 	@GetMapping("/signup")
-	public String getSignUp() {
+	public String getSignUp(Model model) {
+		model.addAttribute("user" ,new User());
 		return "signup";
 	}
 	
 	@PostMapping("/home")
-	public String processPage(@ModelAttribute("user") User user) {
+	public String processPage(@Valid @ModelAttribute("user") User user ,BindingResult result) {
+		if(result.hasErrors()) {
+			System.out.println(result);
+			return "signup";
+		}
+		 
 		System.out.println(user);
 		return "home";
 	}
--- ServerSideValidator/src/main/java/com/Main/Entity/User.java
+++ ServerSideValidator/src/main/java/com/Main/Entity/User.java
@@ -1,12 +1,15 @@
 package com.Main.Entity;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class User {
  @NotBlank(message = "User Name Cann't be Empty!!")
  @Size(min = 8 , max = 15 ,message = "UserName must be in between 8-15!!")
  private String username;
+ @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+		    message = "Password must contain at least 8 characters, including uppercase, lowercase, number, and special character")
  private String password;
 public String getUsername() {
 	return username;
